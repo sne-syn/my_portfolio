19d7076b214659be8baf81b044f6d501c25c334b
sne-syn <36764312+sne-syn@users.noreply.github.com>
2021-02-17 21:27:34 +0100

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,10 +4,14 @@ import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 const Layout = ({ children }) => {
+  const {isOpen, setIsOpen} = React.useState(false);
+  const toggleSidebar = () => {
+    setIsOpn(!isOpen)
+  }
   return (
     <>
-      <Navbar />
-      <Sidebar />
+      <Navbar toggleSidebar={toggleSidebar} />
+      <Sidebar toggleSidebar={toggleSidebar} isOpen={isOpen} />
       {children}
       <Footer />
     </>
