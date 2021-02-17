e80274ef50896b774aaa25c8c2f9ba0ca53ee802
sne-syn <36764312+sne-syn@users.noreply.github.com>
2021-02-17 21:20:39 +0100

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,18 @@ import Links from "../constants/links";
 import SocialLinks from "../constants/socialLinks";
 import { FaTimes } from "react-icons/fa";
 const Sidebar = () => {
-  return <h2>sidebar component</h2>;
+  const isOpen = false;
+  return (
+    <div className={`sidebar ${isOpen ? "show-sidebar" : ""}`}>
+      <button className="close-btn">
+         <FaTimes></FaTimes>
+      </button>
+      <div className="side-container">
+        <Links styleClass="sidebar-links" />
+        <SocialLinks styleClass="sidebar-icons" />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
